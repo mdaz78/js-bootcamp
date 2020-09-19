ed49817e7be467d53b08ba002c3ec9b4f0a3d164
Md Abu Zafar <mdabuzafar78@gmail.com>
2020-09-20 03:18:45 +0530

    [Feat] Learn about extends, super and subclasses

---
+++ Section 19 - Prototypes, Classes & the new Operator/sub-classes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  eat() {
+    return `${this.name} is eating`;
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, age, livesLeft = 9) {
+    super(name, age);
+    this.livesLeft = livesLeft;
+  }
+  meow() {
+    return "MEOWW!!!!";
+  }
+}
+
+class Dog extends Pet {
+  bark() {
+    return "WOOF!!!!";
+  }
+}
